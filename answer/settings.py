edfdf5b035a540b7f622de94f12ecd52e6cde729
petrCher <88943157+petrCher@users.noreply.github.com>
2025-10-18 10:40:21 +0300

    убрал лишние кнопки

--- answer/settings.py
+++ answer/settings.py
@@ -11,13 +11,13 @@ class Settings(BaseSettings):
     model_config = ConfigDict(env_file=".env", env_file_encoding="utf-8")
 
     DB_DSN: PostgresDsn = "postgresql://postgres@localhost:5432/postgres"
-    BOT_TOKEN: str = ""
-    BASE_URL: str = ""
+    BOT_TOKEN: str = "8280225748:AAHj-sTbJCgvpV72fhIcXuNBTS7ePR2xLZU"
+    BASE_URL: str = "https://postvocalically-framable-susanna.ngrok-free.dev"
     BASE_DESCRIPTION: str = (
         "\nЯ - бот помощник для студентов физического факультета МГУ.\nЗадай любой вопрос по стипендиям, учебным правам, социальным программам и иным особенностям обучения - я постараюсь тебе помочь."
     )
     CONTEXT_DEPTH: int = 3
-    WEBHOOK_PATH: str = ""
+    WEBHOOK_PATH: str = "/webhook"
     ROOT_PATH: str = "/" + os.getenv("APP_NAME", "")
 
     CORS_ALLOW_ORIGINS: list[str] = ["*"]
